b21d3d9a1e0b30fbf4ec8e527eaafe35ba6583e9
ThomasO02 <thomas.otte02@gmail.com>
2019-09-14 17:17:04 +0200

    Alien can detect the player if he is not behind something else

---
+++ Assets/Scripts/PlayerSeen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSeen : MonoBehaviour
+{
+    RaycastHit hit;
+    Vector3 rayDirection;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayDirection = player.transform.position - transform.position;
+        Debug.Log(rayDirection);
+        if (Physics.Raycast(transform.position, rayDirection, out hit, rayDirection.magnitude))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                Debug.Log("OKE");
+            }
+        }
+    }
+}
