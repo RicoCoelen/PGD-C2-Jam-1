654b1ee9d33bc4cf916e47ebdcb4e137da1cc947
Joey Smulders <joey_smulders@msn.com>
2019-09-17 17:56:14 +0200

    Added a collectible item and a UI that shows how many collectibles the player has collected

---
+++ Assets/Scripts/CollectCollectible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCollectible : MonoBehaviour
+{
+    HUDScore HUDScore;
+
+    private void Start()
+    {
+        HUDScore = GameObject.Find("UI").GetComponent<HUDScore>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            HUDScore.score++;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/HUDScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDScore : MonoBehaviour
+{
+    public int score;
+    Text collectibleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        collectibleText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(score);
+        collectibleText.text = "Collectibles: " + score;
+    }
+}
